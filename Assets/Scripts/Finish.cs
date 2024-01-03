89fcd7e478cacac199aaf6cb0821931e1ea7de11
Mansi Manoj <ec21843@qmul.ac.uk>
2024-01-03 11:20:59 +0000

    Start Menu, Levels Menu

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -9,9 +9,60 @@ public class Finish : MonoBehaviour
     {
         if(other.gameObject.name == "Player")
         {
+            UnlockNewLevel();
             Debug.Log("Player entered the finish zone.");
+            Debug.Log("Current Level Index: " + SceneManager.GetActiveScene().buildIndex);
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
 
     }
+
+    /*void UnlockNewLevel()
+    {
+        if (SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
+        {
+            PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex + 1);
+            PlayerPrefs.SetInt("UnlockedLevel", PlayerPrefs.GetInt("UnlockedLevel", 1) + 1);
+            Debug.Log("Unlocked Levels after update: " + PlayerPrefs.GetInt("UnlockedLevel"));
+            PlayerPrefs.Save();
+            Debug.Log("New Reached Index: " + PlayerPrefs.GetInt("ReachedIndex"));
+            Debug.Log("New Unlocked Level: " + PlayerPrefs.GetInt("UnlockedLevel"));
+        }
+    }*/
+
+    void UnlockNewLevel()
+{
+    // Get the index of the current scene.
+    int currentLevelIndex = SceneManager.GetActiveScene().buildIndex;
+    
+    // Retrieve the highest level index reached by the player.
+    int reachedIndex = PlayerPrefs.GetInt("ReachedIndex", 0); // Default to 0 if not set.
+
+    // Check if the current level index is greater than the reached index.
+    if (currentLevelIndex > reachedIndex)
+    {
+        // Update the reached index to the current level index.
+        PlayerPrefs.SetInt("ReachedIndex", currentLevelIndex);
+    }
+    
+    // Retrieve the number of unlocked levels.
+    int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1); // Default to 1 if not set.
+
+    // Update the number of unlocked levels if necessary.
+    // This assumes you want to unlock the next level only if the current level is the last unlocked level.
+    if (currentLevelIndex == unlockedLevel)
+    {
+        PlayerPrefs.SetInt("UnlockedLevel", unlockedLevel + 1);
+        Debug.Log("Unlocked Levels after update: " + (unlockedLevel + 1));
+    }
+
+    // Save the changes to PlayerPrefs.
+    PlayerPrefs.Save();
+
+    // Log the new values.
+    Debug.Log("New Reached Index: " + PlayerPrefs.GetInt("ReachedIndex"));
+    Debug.Log("New Unlocked Level: " + PlayerPrefs.GetInt("UnlockedLevel"));
 }
+
+}
+
